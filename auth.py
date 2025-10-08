51be34e958605a7f2dd78d9ec3425b27ded433c0
Kian Santang DEV <93410145+KianSantang777@users.noreply.github.com>
2025-10-08 21:16:32 +0700

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,25 @@
+# Encrypted : Kiansantang DEV
+# Time Encrypted: 08-10-25 21:14 PM
+# Telegram : t.me/xqndrs66 && t.me/xqndrs
+# Github : github.com/KianSantang777
+import sys;assert sys.version_info[0]==3;k4Cbv2tFXTPkfmRRDEl5SR3ZmWeMUXUZHolXIrCNS3CNett5ZXLjeT1X=__import__('base64');s8mfxMdzM1x48HT56vUQGZCv5kZn6KxYlMSQPZ7BfGnBG3FCa7qpqYF69ZDaHykMy=__import__('marshal');hnR6UHD5WdSPUrhkCs3ysks5uTib4xh=__import__('lzma');feW1XX4bXHDEHIKoztkdm5jpXcuvGigHkGOvFDa6MNctQKFC8TQN=__import__('os')
+def fsfRay2kkWBIj8OgJHARVx6mz0ch3KTTMPgsZWaTTU():
+ try:
+  with open(feW1XX4bXHDEHIKoztkdm5jpXcuvGigHkGOvFDa6MNctQKFC8TQN.path.join(feW1XX4bXHDEHIKoztkdm5jpXcuvGigHkGOvFDa6MNctQKFC8TQN.path.dirname(__file__), 'auth.bin'),"rb") as CQmqopWjnFLReqW58rPylzvQcEyM:
+   zOOArOlWG9u3ZeqksHn9rm4JuSCEdT3GbSuUlMtyyWMqRC=CQmqopWjnFLReqW58rPylzvQcEyM.read()[::-1]
+  Qx6Dwm83gqGHlZOxSUpyqIRJBEbla1Ejma87ETwJwXQLKFBmkhBE=k4Cbv2tFXTPkfmRRDEl5SR3ZmWeMUXUZHolXIrCNS3CNett5ZXLjeT1X.b64decode(zOOArOlWG9u3ZeqksHn9rm4JuSCEdT3GbSuUlMtyyWMqRC)
+  w1qFJCBMb8l4Y2BVqbaOdw6D7mbUhEDAGZ9185ENlYG9PiMYwmVpmd=k4Cbv2tFXTPkfmRRDEl5SR3ZmWeMUXUZHolXIrCNS3CNett5ZXLjeT1X.b16decode(Qx6Dwm83gqGHlZOxSUpyqIRJBEbla1Ejma87ETwJwXQLKFBmkhBE)
+  uYwgkI7n7EJNSL3CvZCLjtZOaedAKny4oXV6wwMKu4gYi9Jhzc4y=hnR6UHD5WdSPUrhkCs3ysks5uTib4xh.decompress(w1qFJCBMb8l4Y2BVqbaOdw6D7mbUhEDAGZ9185ENlYG9PiMYwmVpmd)
+  pkYfNgU2afS0GIYNOFHvS3dBS6mE=s8mfxMdzM1x48HT56vUQGZCv5kZn6KxYlMSQPZ7BfGnBG3FCa7qpqYF69ZDaHykMy.loads(uYwgkI7n7EJNSL3CvZCLjtZOaedAKny4oXV6wwMKu4gYi9Jhzc4y)
+  exec(pkYfNgU2afS0GIYNOFHvS3dBS6mE,{"__name__":"__main__"})
+ except FileNotFoundError:
+  dummy = b'gAAAAAA='[::-1]
+  try:
+   with open(feW1XX4bXHDEHIKoztkdm5jpXcuvGigHkGOvFDa6MNctQKFC8TQN.path.join(feW1XX4bXHDEHIKoztkdm5jpXcuvGigHkGOvFDa6MNctQKFC8TQN.path.dirname(__file__), 'auth.bin'), "wb") as d:
+    d.write(dummy)
+  except Exception: pass
+ except Exception as e:
+  try:
+   import traceback; traceback.print_exc()
+  except Exception: pass
+if __name__=="__main__":fsfRay2kkWBIj8OgJHARVx6mz0ch3KTTMPgsZWaTTU()
